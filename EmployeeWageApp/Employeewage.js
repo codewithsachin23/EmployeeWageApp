e32da1f06cfffe0aa1efac47cd782099d1577043
sachin shukla <sachinshukla232003@gmail.com>
2025-03-04 15:10:40 +0530

    [Sachin Shukla]Add. calculate daily wage

--- EmployeeWageApp/Employeewage.js
+++ EmployeeWageApp/Employeewage.js
@@ -1,10 +1,25 @@
-const IS_PRESENT = 1;
+const IS_ABSENT = 0;
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
 
-// Generating random number (0 or 1)
-let empCheck = Math.floor(Math.random() * 2);
+// Generating work type :- Absent, Part-Time, Full-Time
+let empCheck = Math.floor(Math.random() * 3);
 
-if (empCheck === IS_PRESENT) {
-    console.log("Employee is Present");
-} else {
-    console.log("Employee is Absent");
+let empHours = 0;
+
+switch (empCheck) {
+    case IS_PART_TIME:
+        empHours = PART_TIME_HOURS;
+        break;
+    case IS_FULL_TIME:
+        empHours = FULL_TIME_HOURS;
+        break;
+    default:
+        empHours = 0;
 }
+
+let dailyWage = empHours * WAGE_PER_HOUR;
+console.log(`Employee worked ${empHours} hours, Daily Wage: $${dailyWage}`);
